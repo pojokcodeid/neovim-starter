b888a60126a53b2dcc67838c661f0106d27482d6
Pojok Code <pojokcodeid@gmail.com>
2024-11-08 10:22:35 +0700

    fix: disable numbering if load plugins first time

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -4,6 +4,7 @@ if not vim.loop.fs_stat(lazypath) then
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
+vim.opt.number = false
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
